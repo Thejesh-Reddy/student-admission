8666dc480e0d906a2e62ae4ab930a207490d7e47
Thejesh-Reddy <thejeshreddy11@gmail.com>
2025-04-01 12:03:35 +0530

    commit - Percentage

--- src/main/java/com/devops/StudentAdmissionCLI.java
+++ src/main/java/com/devops/StudentAdmissionCLI.java
@@ -12,10 +12,18 @@ public class StudentAdmissionCLI {
 
         System.out.print("Enter Student Age: ");
         int age = scanner.nextInt();
+
+        System.out.print("Enter Student Class: ");
+        int grade = scanner.nextInt();
+
+        System.out.print("Enter Student Percentage: ");
+        int percent = scanner.nextInt();
         
         System.out.println("\nStudent Admission Details:");
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
+        System.out.println("Class: " + grade);
+        System.out.println("Percentage: " + percent);
         
         scanner.close();
     }
